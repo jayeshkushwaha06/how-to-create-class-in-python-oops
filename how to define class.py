9a6d78a5824e4b2ffc32a34a5c6b4eeea11b943c
jayeshkushwaha06 <anshkushwaha072024@gmail.com>
2024-06-20 22:45:52 +0530

    Add files via upload

---
+++ how to define class.py
@@ -0,0 +1,30 @@
+#creating class
+class student:
+  name="karan"
+#creating object (instance)
+
+s1=student()
+print(s1.name)
+
+class Student:
+    name="karan"
+s1 =Student()
+print(s1.name)
+
+s2=Student()
+print(s2.name)
+
+class car:
+    color ="blue"
+
+car1 = car()
+print(car1.color)
+
+
+class car:
+    color ="blue"
+    brand ="mercedes"
+
+car1 = car()
+print(car1.color)
+print(car1.brand)
